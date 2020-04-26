f54b3b84a50bafccb27a49f93b0c07f06f52375d
Ana-Araujo <ana.araujo.ie@gmail.com>
2020-04-26 10:27:17 +0000

    search

--- mobdev-ca2/src/app/pages/quotes/quotes.page.ts
+++ mobdev-ca2/src/app/pages/quotes/quotes.page.ts
@@ -14,7 +14,7 @@ export class QuotesPage implements OnInit {
 
     
     quotes: Observable<any>;
-
+    search:any = "";
     constructor(private router: Router, private api: ApiService) { }
 
     ngOnInit() {
@@ -29,5 +29,9 @@ export class QuotesPage implements OnInit {
         let quoteId = quote.quote_id;  
         this.router.navigateByUrl(`/tabs/quotes/${quote.id}`);
     }
+    filter(quote){
+    // console.log(death.author);
+    return quote.author.toLowerCase().indexOf(this.search.toLowerCase()) != -1;
+  }
 }
 
