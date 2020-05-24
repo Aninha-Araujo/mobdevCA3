96f74079db779d2c5268ca0c1a1e87aa331f6f69
ana carolina sacco araujo <ana.araujo.ie+ana@gmail.com>
2020-05-24 12:39:56 +0000

    heroku

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)},
-  {
-    path: 'camera',
-    loadChildren: () => import('./pages/camera/camera.module').then( m => m.CameraPageModule)
-  }
+
 ];
 
 @NgModule({
